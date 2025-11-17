4bd409a781e8c2f4b470ff4f77496be16dbf91f2
Alicia <94490665+acaruso369@users.noreply.github.com>
2025-11-17 23:17:09 +0000

    home page is coming together

---
+++ src/script.js
@@ -0,0 +1,12 @@
+// Get elements
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("menu");
+
+// Toggle menu
+hamburger.addEventListener("click", () => {
+    if (menu.style.display === "none" || menu.style.display === "") {
+        menu.style.display = "flex";
+    } else {
+        menu.style.display = "none";
+    }
+});
